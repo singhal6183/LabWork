c0da6e5458d58a3eb9f95b729c51673dd53f5d0b
singhal6183 <singhal6183@gmail.com>
2023-06-07 15:10:09 -0400

    week 6 assignment added

--- src/practice/practice.java
+++ src/practice/practice.java
@@ -9,7 +9,7 @@ public class practice {
 	/*Create an arrayList of int called ages that contains the 
 		following values: 3, 9, 23, 64, 2, 8, 28, 93.*/
 
-	List<Integer> myList = new ArrayList<>();
+	/*List<Integer> myList = new ArrayList<>();
 	myList.add(1); // add Elements in arrayList
 	myList.add(2);
 	/*for(Integer names: myList)
@@ -17,14 +17,14 @@ public class practice {
 	System.out.println(names);
 	}*/
 	
-	for(int i=0; i< myList.size(); i++)
+	/*for(int i=0; i< myList.size(); i++)
 	{
 	System.out.println(myList.get(i));
 	}
-	
-	
-    	
-    }
+	*/
+	}
+		public static void doSomething(int a, int b) { }
+    
 }
-   
+
 
